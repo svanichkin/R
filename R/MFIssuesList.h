941560259b1e4084fc032c86874b920eb9317d4b
Сергей Ваничкин <sergey@vanichkin.ru>
2013-04-06 12:54:36 +0400

    Подключение списка задач. С багами.

--- R/MFIssuesList.h
+++ R/MFIssuesList.h
@@ -8,6 +8,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface MFIssuesList : NSTableView <NSTableViewDataSource>
+@interface MFIssuesList : NSTableView <NSTableViewDataSource, NSTableViewDelegate>
 
 @end
--- R/MFIssuesList.m
+++ R/MFIssuesList.m
@@ -14,6 +14,7 @@
 {
     MFSettings *_settings;
     NSArray *_issues;
+    id _oldSelectedCell;
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
@@ -37,6 +38,7 @@
                                                    object:nil];
         _settings = [MFSettings sharedInstance];
         self.dataSource = self;
+        self.delegate = self;
     }
     return self;
 }
@@ -49,37 +51,14 @@
 - (void) projectSelected:(NSNotification *) notification
 {
     _issues = [[MFDatabase sharedInstance] issuesByProjectId:_settings.selectedProjectId];
-        
-    // Генерация таблицы
+
+    [self deselectAll:nil];
+    [self reloadData];
+    
     if (_issues)
     {
-//            self.m
-            
-            /*[self.menu removeAllItems];
-            
-            for (Issue *p in _issues)
-            {
-                NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:p.name
-                                                              action:nil//@selector (projectSelected:)
-                                                       keyEquivalent:@""];
-                item.target = self;
-                item.tag = [p.nid intValue];
-                [self.menu addItem:item];
-            }*/
-            
-            // Сетим выбранный проект
-            //[self selectItemWithTag:[_settings.selectedProjectId intValue]];
-//            self sele
-        }
-        
-//        [self setEnabled:_issues.count];
-        [self setEnabled:YES];
-    [self reloadData];
-        
-//        selectRowIndexes:(NSIndexSet *)indexes byExtendingSelection:
-//        - (NSTableViewSelectionHighlightStyle)selectionHighlightStyle NS_AVAILABLE_MAC(10_5);
-//       - (void)setSelectionHighlightStyle:(NSTableViewSelectionHighlightStyle)selectionHighlightStyle NS_AVAILABLE_MAC(10_5);
-
+//    [self selectCell:<#(NSCell *)#>]
+    }
 }
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
@@ -89,62 +68,35 @@
 
 - (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
 {
-    NSButtonCell *cell = [[NSButtonCell alloc] init];
-    [cell setAllowsMixedState:YES];
-    [(NSButtonCell *)cell setButtonType:NSSwitchButton];
-    [cell setTitle:@"Test"];
+    Issue *issue = [_issues objectAtIndex:row];
     
-    return cell;
+    NSString *type = [NSString stringWithFormat:@"%@ %@ %@", [issue.status.name lowercaseString], [issue.priority.name  lowercaseString], [issue.tracker.name lowercaseString]];
+    
+    NSString *number = [NSString stringWithFormat:@"#%@", issue.nid];
+    
+    return @{@"type":type, @"text":issue.text, @"number":number};
 }
-
-- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
-{
-}
-
-// Выбрали один из проектов
-//- (void) projectSelected:(NSNotification *) notification
-//{
-    // Скрываем правый фрейм
-    /*[_mainPageScroll setHidden:YES];
-     
-     // Скрываем нажатую ячейку
-     if (_oldCellSelected)
-     {
-     [[_oldCellSelected viewWithTag:1] setHidden:NO];
-     [[_oldCellSelected viewWithTag:2] setHidden:YES];
-     }
-     
-     _projectSelected = item;
-     
-     // Загрузка задач по проекту
-     RKProject *projects = [_projects objectAtIndex:item.tag];
-     _issues = projects.issues;
-     
-     NSUInteger count = [_issuesArrayController.arrangedObjects count];
-     [_issuesArrayController removeObjectsAtArrangedObjectIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0,count)]];
-     
-     for (RKIssue *i in _issues)
-     {
-     if ([_filtersControl checkIssueWithStatusIndex:[i.status.index intValue]
-     priorityIndex:[i.priority.index intValue]
-     andTrackerIndex:[i.tracker.index intValue]])
-     {
-     
-     NSString *type = [NSString stringWithFormat:@"%@ %@ %@", [i.status.name lowercaseString], [i.priority.name  lowercaseString], [i.tracker.name lowercaseString]];
-     
-     [_issuesArrayController addObject:@{@"text":[NSString stringWithFormat:@"%@", i.subject],
-     @"type":type,
-     @"number":[NSString stringWithFormat:@"#%@", i.index]}];
-     }
-     }
-     [_issuesTable reloadData];
-     [_issuesTable deselectAll:nil];
-     */
-//}
 
 - (void) redrawIssues
 {
     
+}
+
+- (void)tableViewSelectionIsChanging:(NSNotification *)aNotification
+{
+    // Скрываем нажатую ячейку
+    if (_oldSelectedCell)
+    {
+        [[_oldSelectedCell viewWithTag:1] setHidden:NO];
+        [[_oldSelectedCell viewWithTag:2] setHidden:YES];
+    }
+    
+    NSTableCellView *selectedCell = [self viewAtColumn:0 row:self.selectedRow makeIfNecessary:YES];
+    
+    _oldSelectedCell = selectedCell;
+    
+    [[selectedCell viewWithTag:1] setHidden:YES];
+    [[selectedCell viewWithTag:2] setHidden:NO];
 }
 
 - (void) issueSelected:(NSMenuItem *)sender
